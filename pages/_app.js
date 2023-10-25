1a54fdc13367d80a750df8348346a3ccb54b445d
vivek anjana <vivekanjana76@gmail.com>
2023-10-25 12:02:55 +0530

    google oauth and mongodb atlas setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component, pageProps: { session, ...pageProps }
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps}/>
+    </SessionProvider>
+  )
 }
 
 import { useEffect } from 'react';
@@ -15,5 +22,5 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+
 
